45bfbdc2d1dff7100923ce72db753e8c108dde59
YJJfish <3190102352@zju.edu.cn>
2022-05-12 20:57:50 +0800

    Update ArgParser

--- include/ArgsParser.h
+++ include/ArgsParser.h
@@ -3,60 +3,37 @@
 #include <map>
 #include <string>
 
-class ArgsParser
-{
+class ArgsParser{
 	std::vector<std::string> args;
 	std::map<std::string, std::string> argMap;
 
-	void parseArgments()
-	{
-		for (int i = 0; i < args.size(); i++)
-		{
-			if (args[i][0] == '-')
-			{
+	void parseArgments(){
+		for (int i = 0; i < args.size(); i++){
+			if (args[i][0] == '-'){
 				std::string name(&args[i][1]);
-				if (i + 1 < args.size())
-				{
+				if (i + 1 < args.size()){
 					argMap[name] = args[i + 1];
 					i++;
-					//std::cout << name << ": " << argMap[name] << std::endl;
 				}
 			}
 		}
 	}
-	template <typename T>
-	T convertStringToValue(std::string str) const
-	{
-		return (T)std::stod(str);
-	}
-	template <> float convertStringToValue(std::string str) const{ return std::stof(str); }
-	template <> int convertStringToValue(std::string str) const{ return std::stoi(str); }
-	template <> std::string convertStringToValue(std::string str)const { return str; }
-	template <> bool convertStringToValue(std::string str) const
-	{
-		if (str == "true") return true;
-		if (str == "false") return false;
-		return convertStringToValue<int>(str) != 0;
-	}
+	
 
 public:
-	ArgsParser(){}
-	ArgsParser(int argn, const char **args)
-	{
-		for (int i = 0; i < argn; i++) {
+	ArgsParser() {}
+	ArgsParser(int argn, const char** args){
+		for (int i = 0; i < argn; i++)
 			this->args.push_back(args[i]);
-		}
 		parseArgments();
 	}
-	ArgsParser(const std::vector<std::string>& args)
-	{
+	ArgsParser(const std::vector<std::string>& args) {
 		this->args = args;
 		parseArgments();
 	}
 
 	template <typename T>
-	bool TryGetArgment(std::string argName, T& value) const
-	{
+	bool TryGetArgment(std::string argName, T& value) const{
 		auto it = argMap.find(argName);
 		if (it == argMap.end())
 			return false;
@@ -65,3 +42,16 @@ public:
 		return true;
 	}
 };
+
+template <typename T>
+T convertStringToValue(std::string str) {
+	return (T)std::stod(str);
+}
+template <> float convertStringToValue(std::string str) { return std::stof(str); }
+template <> int convertStringToValue(std::string str) { return std::stoi(str); }
+template <> std::string convertStringToValue(std::string str) { return str; }
+template <> bool convertStringToValue(std::string str) {
+	if (str == "true") return true;
+	if (str == "false") return false;
+	return convertStringToValue<int>(str) != 0;
+}
